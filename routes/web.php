0826bf68ceb6b21096d5473303efdaf9be50768e
midoz42 <meladwilliam420@gmail.com>
2026-08-04 10:09:37 +0300

    adding breeze for auth.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', [BookController::class, 'home'])->name('index');
 Route::get('/books', [BookController::class, 'books'])->name('books');
@@ -20,3 +22,17 @@ Route::get('/test', function () {
         'otherData' => request('otherData', 'hi'),
     ]);
 })->name('test');
+
+//// login and authentication
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
